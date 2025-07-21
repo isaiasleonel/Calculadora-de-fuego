92541108bf6f4ae5611cfae798c086052a5d913a
Isaias Leonel Sardina <isaias.sardina@gmail.com>
2025-07-21 19:24:14 -0300

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+  const env = loadEnv(mode, ".", "");
+  return {
+    define: {
+      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+      // allowedHosts: [
+      //   'eeb4b162-dc88-4652-968d-a7e195493e43-00-23yomms3q8rd7.janeway.replit.dev',
+      //   '.replit.dev',
+      //   'localhost'
+      // ]
+    },
+  };
 });
